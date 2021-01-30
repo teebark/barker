3434a8f5fa6cfc88c454c00776eab6fc6d9992e1
teebark <terry@teebark.com>
2021-01-30 14:48:01 -0500

    added mobile version

--- functions.php
+++ functions.php
@@ -10,9 +10,23 @@ function page_class($classes) {
    // return the $classes array
    return $classes;
 }
+/* Add category to single post pages */
+add_filter('body_class','add_category_to_single');
+function add_category_to_single($classes) {
+	if (is_single() ) {
+		global $post;
+		foreach((get_the_category($post->ID)) as $category) {
+			echo $category->cat_name . ' ';
+			// add category slug to the $classes array
+			$classes[] = 'category-'.$category->slug;
+		}
+	}
+	// return the $classes array
+	return $classes;
+}
 /* Add custom field image-link to featured image as external link */
 add_filter('post_thumbnail_html', 'custom_thumbnail_tag_filter', 10, 3);
-function custom_thumbnail_tag_filter($html, $postid, $thumbnailid, $size) {
+function custom_thumbnail_tag_filter($html, $postid, $thumbnailid) {
 	/* print_r ("html = " . $html,true); */
     if ($thumbnailid) {
 		/* $id = get_post_thumbnail_id(); */
@@ -20,10 +34,17 @@ function custom_thumbnail_tag_filter($html, $postid, $thumbnailid, $size) {
         $src = get_post_meta($postid, 'image_link', true);
 		if ($src) {
 			if ($html) {
-				$html = '<a href="' . $src . '"> <img src="' . $html . '"></a>';}
+				// $show_var = 'html = ' . print_r($html,1);
+				// error_log($show_var);
+				$html = '<a href="' . $src . '"> <img "' . $html . '</a>';}
 		}
     }
     return $html;
+}
+/* Get rid of secondary hamburger menu */
+add_action( 'wp_enqueue_scripts', 'generate_dequeue_secondary_nav_mobile', 999 );
+function generate_dequeue_secondary_nav_mobile() {
+   wp_dequeue_style( 'generate-secondary-nav-mobile' );
 }
 /* Shows filters
 global $wp_filter;
